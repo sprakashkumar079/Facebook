8ba28c24d97d5312880728e9c04aaadda40ebbe8
Prakash <prakash8128154996@gmail.com>
2022-01-05 13:29:22 +0530

    resolve indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 #appliction controller for project
 class ApplicationController < ActionController::Base
+    #this is simple action call for first time
 	def hello
       render html: "hello, world!"
     end
