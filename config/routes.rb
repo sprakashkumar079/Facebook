a0275a915142cc92f127bbaf997804e519012e70
Prakash <prakash8128154996@gmail.com>
2022-01-06 17:03:44 +0530

    adding comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
-  
+  #device route for all like sign in sing up etc.
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root route for first page of project
+  #micropost route
   resources :microposts, only: [:create, :destroy]
+  #home route
   root 'static_pages#home'
 end
