af1fc71e2c24179cdfbf298e68cc4d6ae9fe7cf2
sprakashkumar079 <singh.prakashkumar@ongraphmail.com>
2022-01-07 16:14:44 +0530

    solve indendation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,5 @@
-#home controller for first page 
+#home controller for first page
 class StaticPagesController < ApplicationController
 	#if user is login than the micropost create
 	#root_path
-	def home
-		if sign_in?
-      @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
-	end
 end
