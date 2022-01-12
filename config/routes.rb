d679d5342312799ffa624f877d862ef6796c8f68
sprakashkumar079 <singh.prakashkumar@ongraph.com>
2022-01-12 11:17:18 +0530

    Some changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   
-  get 'likes/create'
-  get 'comments/new'
-  get 'comments/create'
+
   #device route for all like sign in sing up etc.
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root route for first page of project
   #micropost route
   resources :microposts, only: [:create, :destroy]
+  #for like
+  resources :posts, only: [:index, :new, :create, :show, :destroy] do
+      resources :likes, only: [:create]
+  end
+  #for comments
+  resource :comments, only: [:new, :create, :destroy] do
+    resources :likes, only: [:create]
+  end
   #home route
   root 'static_pages#home'
 end
